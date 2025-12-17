1b90c60f1e6a2b412821aa75588d00350dc73419
zaraa-kuki <azfara2006@gmail.com>
2025-12-17 13:00:19 +0700

    Refactor schedule display logic in jadwalutama.php
    Simplify query and update table structure for displaying schedules.

--- jadwalutama.php
+++ jadwalutama.php
@@ -7,11 +7,10 @@ $where_clause = "";
 
 if (!empty($keyword)) {
     $safe_keyword = $conn->real_escape_string($keyword); 
-    
     $where_clause = " WHERE 
-        mk.nama_mk LIKE '%$safe_keyword%' OR 
-        d.nama_dosen LIKE '%$safe_keyword%' OR 
-        jk.ruang LIKE '%$safe_keyword%'";
+        matkul LIKE '%$safe_keyword%' OR 
+        dosen LIKE '%$safe_keyword%' OR 
+        hari LIKE '%$safe_keyword%'";
 }
 
 ?>
@@ -83,79 +82,49 @@ if (!empty($keyword)) {
             Setiap perubahan yang dilakukan pada halaman "Kelola Jadwal" akan otomatis diperbarui ditabel ini.
         </p>
         <table class="jadwal-table">
-            <thead>
-                <tr>
-                    <th style="color: black;">Hari</th>
-                    <th style="color: black;">Jam</th>
-                    <th style="color: black;">Ruang</th>
-                    <th style="color: black;">Mata Kuliah</th>
-                    <th style="color: black;">Dosen</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $query = "SELECT 
-                                jk.hari, 
-                                jk.waktu_mulai, 
-                                jk.waktu_selesai, 
-                                jk.ruang, 
-                                mk.nama_mk, 
-                                d.nama_dosen
-                            FROM 
-                                jadwal_kuliah jk
-                            JOIN
-                                mata_kuliah mk ON jk.id_mk = mk.id_mk
-                            JOIN
-                                dosen d ON jk.id_dosen = d.id_dosen
-                            $where_clause 
-                            ORDER BY 
-                                FIELD(jk.hari, 'SENIN', 'SELASA', 'RABU', 'KAMIS', 'JUMAT', 'SABTU', 'MINGGU'), 
-                                jk.waktu_mulai";
+        <thead>
+    <tr style="background-color: #ffeb3b;">
+        <th style="color: black;">Hari</th>
+        <th style="color: black;">Jam</th>
+        <th style="color: black;">Mata Kuliah</th>
+        <th style="color: black;">Dosen</th>
+    </tr>
+</thead>
+<tbody>
+    <?php
+    // Query disederhanakan tanpa JOIN
+    $query = "SELECT * FROM jd_utama $where_clause 
+          ORDER BY FIELD(hari, 'SENIN', 'SELASA', 'RABU', 'KAMIS', 'JUMAT', 'SABTU', 'MINGGU'), jam_mulai";
                             
-                $result = $conn->query($query);
+    $result = $conn->query($query);
+    $hari_sebelumnya = "";
+    
+    if ($result && $result->num_rows > 0) {
+        while($data = $result->fetch_assoc()) {
+            echo "<tr>";
+            
+            // LOGIKA ROWSPAN (Dihitung berdasarkan tabel jd_utama)
+            if ($data['hari'] != $hari_sebelumnya) {
+                $rowspan_query = "SELECT COUNT(*) AS total FROM jd_utama WHERE hari = '{$data['hari']}' $where_clause";
+                $rowspan_result = $conn->query($rowspan_query);
+                $rowspan_data = $rowspan_result->fetch_assoc();
+                $rowspan = $rowspan_data['total'];
                 
-                $hari_sebelumnya = "";
-                
-                if ($result && $result->num_rows > 0) {
-                    while($data = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        
-                        // --- LOGIKA ROWSPAN ---
-                        if ($data['hari'] != $hari_sebelumnya) {
-                            // 2. Perbarui Query Rowspan ($rowspan_query)
-                            // Query count juga harus menyisipkan filter $where_clause dan JOIN
-                            $rowspan_query = "SELECT COUNT(*) AS total FROM jadwal_kuliah jk
-                                            JOIN mata_kuliah mk ON jk.id_mk = mk.id_mk
-                                            JOIN dosen d ON jk.id_dosen = d.id_dosen
-                                            WHERE jk.hari = '{$data['hari']}' $where_clause";
-                                            
-                            $rowspan_result = $conn->query($rowspan_query);
-                            $rowspan_data = $rowspan_result->fetch_assoc();
-                            $rowspan = $rowspan_data['total'];
-                            
-                            if ($rowspan > 0) {
-                                echo "<td rowspan='{$rowspan}'>{$data['hari']}</td>";
-                                $hari_sebelumnya = $data['hari'];
-                            }
-                        }
-                        // ----------------------------------------------------------
+                echo "<td rowspan='{$rowspan}' style='vertical-align:middle; font-weight:bold;'>{$data['hari']}</td>";
+                $hari_sebelumnya = $data['hari'];
+            }
 
-                        // Mencetak kolom lainnya
-                        echo "<td>" . substr($data['waktu_mulai'], 0, 5) . " - " . substr($data['waktu_selesai'], 0, 5) . "</td>";
-                        echo "<td>{$data['ruang']}</td>";
-                        echo "<td>{$data['nama_mk']}</td>";
-                        echo "<td>{$data['nama_dosen']}</td>";
-                        
-                        echo "</tr>";
-                    }
-                } else {
-                    // Tampilkan pesan yang lebih informatif
-                    echo "<tr><td colspan='5'>Tidak ada data jadwal yang ditemukan";
-                    if (!empty($keyword)) echo " untuk kata kunci '{$keyword}'";
-                    echo ".</td></tr>";
-                }
-                ?>
-            </tbody>
+            // Menampilkan data (Nama kolom disesuaikan: jam, matkul, dosen)
+            echo "<td>{$data['jam_mulai']}</td>";
+            echo "<td>{$data['matkul']}</td>";
+            echo "<td>{$data['dosen']}</td>";
+            echo "</tr>";
+        }
+    } else {
+        echo "<tr><td colspan='4' class='text-center'>Jadwal tidak ditemukan.</td></tr>";
+    }
+    ?>
+</tbody>
         </table>
     </div>
     <!-- Footer -->
