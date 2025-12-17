ac5738e6f9a54db4af08a8bd5320f8831b65b9f6
zaraa-kuki <azfara2006@gmail.com>
2025-12-17 11:32:28 +0700

    Remove redundant database connection code

--- koneksi.php
+++ koneksi.php
@@ -11,15 +11,3 @@ if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
 }
 ?>
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "db_jadwal";
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-// cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
-?>
