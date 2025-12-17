d88862370a74b0c0aafd7532fef1fe52a31121af
zaraa-kuki <azfara2006@gmail.com>
2025-12-17 13:14:58 +0700

    Update kelolajadwal.php

--- kelolajadwal.php
+++ kelolajadwal.php
@@ -40,9 +40,9 @@
     <!-- MENU POP-UP -->
     <div class="popup-menu" id="menuPopup">
         <ul>
-            <li><a href="dashboard.html">Dashboard</a></li>
-            <li><a href="kelolajadwal.html">Kelola Jadwal</a></li>
-            <li><a href="jadwalutama.html">Jadwal Utama</a></li>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="kelolajadwal.php">Kelola Jadwal</a></li>
+            <li><a href="jadwalutama.php">Jadwal Utama</a></li>
         </ul>
     </div>
 
