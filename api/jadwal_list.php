9c776105673e30fb3ee5cd0c0e8982964e5571ad
crival24-a <cristhvaldo@gmail.com>
2025-12-19 11:16:34 +0700

    perbaikan

--- api/jadwal_list.php
+++ api/jadwal_list.php
@@ -1,6 +1,6 @@
 <?php
-header("Content-Type: application/json");
-require_once __DIR__ . '/../koneksi.php';
+header('Content-Type: application/json; charset=utf-8');
+require_once __DIR__ . '/db.php';
 
 $rows = $pdo->query("
   SELECT jk.id, jk.tanggal, jk.catatan,
--- api/jadwal_update_catatan.php
+++ api/jadwal_update_catatan.php
@@ -1,6 +1,6 @@
 <?php
-header("Content-Type: application/json");
-require_once __DIR__ . '/../koneksi.php';
+header('Content-Type: application/json; charset=utf-8');
+require_once __DIR__ . '/db.php';
 
 $input = json_decode(file_get_contents("php://input"), true);
 $id = intval($input["id"] ?? 0);
