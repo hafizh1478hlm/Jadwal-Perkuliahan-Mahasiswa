8b2b445b45a947657e65dc934995fb476ca3bf0b
crival24-a <cristhvaldo@gmail.com>
2026-01-04 18:56:26 +0700

    tambahan bagian JSON

--- api/mata_kuliah_list.php
+++ api/mata_kuliah_list.php
@@ -1,14 +1,25 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+
 require_once '../koneksi.php';
 
-$result = mysqli_query($koneksi, "SELECT matkul FROM mata_kuliah ORDER BY matkul ASC");
+if (!isset($koneksi) && isset($conn)) {
+    $koneksi = $conn;
+}
+
+if (!isset($koneksi)) {
+    echo json_encode([]);
+    exit;
+}
+
+$sql = "SELECT matkul FROM mata_kuliah ORDER BY matkul ASC";
+$query = mysqli_query($koneksi, $sql);
 
 $data = [];
-if ($result) {
-  while ($row = mysqli_fetch_assoc($result)) {
-    $data[] = $row; // <- INI bikin array
-  }
+if ($query) {
+    while ($row = mysqli_fetch_assoc($query)) {
+        $data[] = $row;
+    }
 }
 
 echo json_encode($data);
